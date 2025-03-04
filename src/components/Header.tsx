1e991b0678d401a11e0af55b2510d0d53eb573c7
support-firmos <support@firmos.app>
2025-03-05 04:50:57 +0800

    changed logo size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,12 +7,12 @@ export default function Header() {
     <header className="bg-background py-5">
       <div className="container mx-auto flex flex-col items-center">
         <Link href="/">
-          <div className="relative w-20 h-20">
+          <div className="relative w-50 h-50">
             <Image 
               src="https://storage.googleapis.com/firmos-pics/FirmOS%20Logo%20-%20White.png"
               alt="FirmOS Logo" 
-              width={80}
-              height={80}
+              width={200}
+              height={200}
               className="object-contain"
               unoptimized={true}
             />
